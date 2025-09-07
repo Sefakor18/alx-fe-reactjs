bbb4f01fe95922dd31dca1f072cf9d288e60a0d8
Sefakor18 <sefakoragor876@gmail.com>
2025-09-07 21:20:35 +0000

    yeah

--- my-company/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#222', color: 'white', padding: '10px', textAlign: 'center', marginTop: '20px' }}>
-      <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
